f87b7d0c92d44b36b9918c1f1e4d38a51d9888fd
Ajaymithireddy <166367156+Ajaymithireddy@users.noreply.github.com>
2024-06-01 13:45:35 +0530

    Update employer.service.ts

--- src/app/Service/employer.service.ts
+++ src/app/Service/employer.service.ts
@@ -9,7 +9,7 @@ import { Application } from '../Model/application';
 })
 export class EmployerService {
 
-  base_url = "http://4.242.76.118:8082/";
+  base_url = "http://20.53.113.233:8082/";
   constructor(private http: HttpClient) {   }
 
   public getJob(Id:number):Observable <any>{
